0332a259d6ed2201f82b44098f444e6111757af4
Marcin Biernat <mb@marcinbiernat.pl>
2020-04-13 21:17:49 +0200

    Sample sim

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-import("../pkg/index.js").catch(console.error);
+import("../pkg/index.js").catch(console.error).then((mod) => {
+  const game = mod.Game.new();
+
+  setInterval(() => console.log(game.tick().map(({x, y}) => ({x, y}))), 1000);
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+use js_sys;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
+mod sim;
+
+use sim::*;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,7 +14,6 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -19,9 +22,36 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
 
     Ok(())
+}
+
+#[wasm_bindgen]
+pub struct Game {
+    simulation: Simulation,
+}
+
+/// Public methods, exported to JavaScript.
+#[wasm_bindgen]
+impl Game {
+    pub fn new() -> Game {
+        let mut simulation = Simulation::new();
+
+        simulation.setup();
+
+        Game { simulation }
+    }
+
+    pub fn tick(&mut self) -> js_sys::Array {
+        console::log_1(&JsValue::from_str("game tick"));
+        let events = self.simulation.tick();
+        events.into_iter().map(JsValue::from).collect()
+    }
+
+    pub fn callback(&self, f: js_sys::Function) {
+        console::log_1(&JsValue::from_str("before"));
+        f.call0(&JsValue::NULL).unwrap();
+    }
 }
