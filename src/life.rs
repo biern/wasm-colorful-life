9a7dd1d75fc37a0ff1e57160389891e8e1ff11f1
Marcin Biernat <mb@marcinbiernat.pl>
2020-04-15 20:13:02 +0200

    Board implements Cells

--- src/life.rs
+++ src/life.rs
@@ -12,56 +12,10 @@ pub struct Board {
     size: usize,
 }
 
-impl Board {
-    pub fn new(size: usize) -> Board {
-        Board {
-            size,
-            cell_map: HashMap::new(),
-        }
-    }
-
-    pub fn insert(&mut self, coord: Coord, color: Color) {
-        self.cell_map.insert(coord, color);
-    }
-
-    pub fn tick(&mut self) -> Vec<CellEvent> {
-        let mut updates: Vec<CellEvent> = vec![];
-
-        for CellView { coords, color } in self.cell_map.cells().iter() {
-            let neighbours = self.cell_map.get_neighbours(coords);
-            if neighbours.len() < 2 || neighbours.len() > 3 {
-                updates.push(CellEvent::Died(Cell {
-                    coords: (*coords).clone(),
-                    color: (*color).clone(),
-                }))
-            }
-        }
-
-        for coords in self.cell_map.all_dead_neighbours() {
-            let alive_neighbours = self.cell_map.get_neighbours(&coords);
-            if alive_neighbours.len() == 3 {
-                updates.push(CellEvent::Born(Cell {
-                    coords,
-                    color: Color(1.0, 1.0, 1.0),
-                }))
-            }
-        }
-
-        for update in &updates {
-            match update {
-                CellEvent::Born(cell) => self
-                    .cell_map
-                    .insert(cell.coords.clone(), cell.color.clone()),
-                CellEvent::Died(cell) => self.cell_map.remove(&cell.coords),
-            };
-        }
-
-        updates
-    }
-}
-
 trait Cells {
     fn get(&self, coord: &Coord) -> Option<CellView>;
+    fn insert(&mut self, coord: Coord, color: Color);
+    fn remove(&mut self, coord: &Coord);
     fn get_neighbours(&self, coord: &Coord) -> Vec<CellView>;
     fn all_dead_neighbours(&self) -> Vec<Coord>;
     fn cells(&self) -> Vec<CellView>;
@@ -78,17 +32,28 @@ static NEIGHBOURS_DIFFS: &'static [(i32, i32)] = &[
     (-1, 1),
 ];
 
-impl Cells for HashMap<Coord, Color> {
+impl Cells for Board {
     fn get(&self, coords: &Coord) -> Option<CellView> {
-        self.get_key_value(coords)
+        self.cell_map
+            .get_key_value(coords)
             .map(|(coords, color)| CellView { coords, color })
+    }
+
+    fn remove(&mut self, coords: &Coord) {
+        self.cell_map.remove(coords);
+    }
+
+    fn insert(&mut self, coords: Coord, color: Color) {
+        self.cell_map.insert(coords, color);
     }
 
     fn get_neighbours(&self, coord: &Coord) -> Vec<CellView> {
         NEIGHBOURS_DIFFS
             .iter()
             .filter_map(|(dx, dy)| {
-                let (coords, color) = self.get_key_value(&Coord(coord.0 + dx, coord.1 + dy))?;
+                let (coords, color) = self
+                    .cell_map
+                    .get_key_value(&Coord(coord.0 + dx, coord.1 + dy))?;
 
                 Some(CellView { coords, color })
             })
@@ -97,6 +62,7 @@ impl Cells for HashMap<Coord, Color> {
 
     fn all_dead_neighbours(&self) -> Vec<Coord> {
         let possible_empty: HashSet<Coord> = self
+            .cell_map
             .keys()
             .flat_map(|coords| {
                 NEIGHBOURS_DIFFS
@@ -107,7 +73,7 @@ impl Cells for HashMap<Coord, Color> {
 
         possible_empty
             .into_iter()
-            .filter_map(|coords| match self.get(&coords) {
+            .filter_map(|coords| match self.cell_map.get(&coords) {
                 Some(_) => None,
                 None => Some(coords),
             })
@@ -115,12 +81,55 @@ impl Cells for HashMap<Coord, Color> {
     }
 
     fn cells(&self) -> Vec<CellView> {
-        self.iter()
+        self.cell_map
+            .iter()
             .map(|(coord, color)| CellView {
                 coords: coord,
                 color,
             })
             .collect::<Vec<CellView>>()
+    }
+}
+
+impl Board {
+    pub fn new(size: usize) -> Board {
+        Board {
+            size,
+            cell_map: HashMap::new(),
+        }
+    }
+
+    pub fn tick(&mut self) -> Vec<CellEvent> {
+        let mut updates: Vec<CellEvent> = vec![];
+
+        for CellView { coords, color } in self.cells().iter() {
+            let neighbours = self.get_neighbours(coords);
+            if neighbours.len() < 2 || neighbours.len() > 3 {
+                updates.push(CellEvent::Died(Cell {
+                    coords: (*coords).clone(),
+                    color: (*color).clone(),
+                }))
+            }
+        }
+
+        for coords in self.all_dead_neighbours() {
+            let alive_neighbours = self.get_neighbours(&coords);
+            if alive_neighbours.len() == 3 {
+                updates.push(CellEvent::Born(Cell {
+                    coords,
+                    color: Color(1.0, 1.0, 1.0),
+                }))
+            }
+        }
+
+        for update in &updates {
+            match update {
+                CellEvent::Born(cell) => self.insert(cell.coords.clone(), cell.color.clone()),
+                CellEvent::Died(cell) => self.remove(&cell.coords),
+            };
+        }
+
+        updates
     }
 }
 
@@ -165,7 +174,7 @@ mod tests {
 
         board.tick();
 
-        assert_eq!(board.cell_map.cells(), vec![])
+        assert_eq!(board.cells(), vec![])
     }
 
     #[test]
