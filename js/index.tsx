004449f4a2ca3eabfc9b9a26dc532599185f625a
Marcin Biernat <mb@marcinbiernat.pl>
2020-04-19 17:22:16 +0200

    Update to use react refs

--- js/index.tsx
+++ js/index.tsx
@@ -1,63 +1,72 @@
 import * as R from "ramda";
 import ReactDOM from "react-dom";
-import React, { useState, useEffect } from "react";
-import { EventEmitter } from "events";
-import styled from "styled-components";
+import React, { useEffect, useRef, MutableRefObject } from "react";
 
 const wasm = import("../pkg/index.js");
 
+type RefMap = { [key: string]: MutableRefObject<HTMLTableCellElement | null> };
+
 export const App = () => {
-  const [emitter, setEmitter] = useState<EventEmitter | undefined>(undefined);
-  const size = 30;
+  const size = 50;
+  const updatesPerRender = 1;
+
+  const refs: RefMap = {};
+
+  for (const i of R.range(0, size)) {
+    for (const j of R.range(0, size)) {
+      refs[`${i}-${j}`] = useRef(null);
+    }
+  }
 
   useEffect(() => {
     const run = async () => {
-      const publishEvents = (events: Event[]) =>
-        events.forEach((e) => emitter.emit("cell", e));
-
-      const emitter = new EventEmitter();
-
-      setEmitter(emitter);
-
       const _mod = await wasm;
       const mod = _mod as Exclude<typeof _mod, void>;
 
       const game = mod.Game.new(size);
 
-      publishEvents(JSON.parse(game.get_state()));
+      const eventHandler = updateCell(refs);
+      const handleEvents = (events: Event[]) => events.forEach(eventHandler);
+
+      handleEvents(JSON.parse(game.get_state()));
 
       setInterval(() => {
         console.time("tick");
-        const events = game.tick();
+        const events = R.range(0, updatesPerRender).map(() => game.tick());
         console.timeEnd("tick");
 
-        console.time("decode");
-        const decode = JSON.parse(events);
-        console.timeEnd("decode");
+        console.time("JSON parse");
+        const decoded = R.flatten(events.map((el) => JSON.parse(el)));
+        console.timeEnd("JSON parse");
 
         console.time("publish");
-        publishEvents(decode);
+        handleEvents(decoded);
         console.timeEnd("publish");
-      }, 500);
+      }, 200);
     };
 
     run();
   }, []);
 
-  return emitter ? (
-    <table style={{ borderCollapse: "collapse" }}>
-      <tbody>
-        {R.range(0, size).map((i) => (
-          <tr key={i}>
-            {R.range(0, size).map((j) => (
-              <Cell key={`${i}-${j}`} x={i} y={j} emitter={emitter} />
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  ) : (
-    <div />
+  return (
+    <div>
+      <table style={{ borderCollapse: "collapse" }}>
+        <tbody>
+          {R.range(0, size).map((i) => (
+            <tr key={i}>
+              {R.range(0, size).map((j) => (
+                <Cell
+                  key={`${i}-${j}`}
+                  x={i}
+                  y={j}
+                  cellRef={refs[`${i}-${j}`]}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
@@ -68,64 +77,41 @@ type CellData = {
 
 type Event = { Died: CellData } | { Born: CellData };
 
-type CellProps = { x: number; y: number; emitter: EventEmitter };
+type CellProps = {
+  x: number;
+  y: number;
+  cellRef: MutableRefObject<HTMLTableCellElement | null>;
+};
 
-type CellState =
-  | {
-      kind: "Dead";
-    }
-  | { kind: "Alive"; color: [number, number, number] };
-
-/* const CellDisplay = styled.td<{ cell: CellState }>`
- *   width: 1rem;
- *   height: 1rem;
- *   background-color: ${(props) =>
- *     props.cell.kind === "Dead"
- *       ? "none"
- *       : `rgba(${props.cell.color.map((c) => c * 255)})`};
- * `;
- *  */
-const Cell = ({ x, y, emitter }: CellProps) => {
-  const [state, setState] = useState<CellState>({ kind: "Dead" });
-
-  useEffect(() => {
-    const listener = (e: Event) => {
-      const data = "Died" in e ? e.Died : e.Born;
-
-      if (data.coords[0] === x && data.coords[1] === y) {
-        /* setTimeout(() => setState("Died" in e ? "Died" : "Born"), 0); */
-        setTimeout(
-          setState(
-            "Died" in e
-              ? { kind: "Dead" }
-              : { kind: "Alive", color: data.color }
-          ),
-          0
-        );
-      }
-    };
-
-    emitter.addListener("cell", listener);
-
-    return () => {
-      emitter.removeListener("cell", listener);
-    };
-  }, [emitter]);
-
-  /* return <CellDisplay cell={state} />; */
+const Cell = ({ x, y, cellRef }: CellProps) => {
   return (
     <td
       style={{
         width: "1rem",
+        minWidth: "1rem",
         height: "1rem",
-        transition: "background-color 0.5s",
-        backgroundColor:
-          state.kind === "Dead"
-            ? "transparent"
-            : `rgba(${state.color.map((c) => c * 255)})`,
+        minHeight: "1rem",
       }}
+      ref={cellRef}
     ></td>
   );
+};
+
+const updateCell = (refs: RefMap) => (e: Event) => {
+  const data = "Died" in e ? e.Died : e.Born;
+  const element = refs[`${data.coords[0]}-${data.coords[1]}`]?.current;
+
+  if (!element) {
+    console.log(refs, `${data.coords[0]}-${data.coords[1]}`);
+
+    console.log("Invalid data", data.coords);
+    return;
+  }
+  if ("Died" in e) {
+    element.style.backgroundColor = "transparent";
+  } else {
+    element.style.backgroundColor = `rgb(${data.color.map((c) => c * 255)})`;
+  }
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
