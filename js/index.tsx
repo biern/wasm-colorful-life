985c745bed93c2756ebb1234060f3af8f60bb5e3
Marcin Biernat <mb@marcinbiernat.pl>
2020-04-23 18:58:35 +0200

    Threejs

--- js/index.tsx
+++ js/index.tsx
@@ -1,6 +1,8 @@
 import * as R from "ramda";
 import ReactDOM from "react-dom";
 import React, { useEffect, useRef, MutableRefObject, useState } from "react";
+import { Canvas, useFrame, useThree } from "react-three-fiber";
+import { Mesh, MeshStandardMaterial, Color } from "three";
 
 import type { Game } from "../pkg/index";
 
@@ -12,7 +14,7 @@ const useLife = (args: {
   eventHandler: (event: Event) => void;
 }) => {
   const updatesPerRender = 1;
-  const [fps, setFps] = useState(3);
+  const [fps, setFps] = useState(args.fps);
   const [game, setGame] = useState<undefined | Game>(undefined);
   const [size, setSize] = useState(args.size);
 
@@ -76,79 +78,105 @@ const useLife = (args: {
   };
 };
 
+type MeshNode = JSX.IntrinsicElements["mesh"];
+
 export const App = () => {
-  const nodes = useRef<{ [key: string]: HTMLElement }>({});
-  const setNode = (key: string) => (node: HTMLElement) => {
+  const mouse = useRef<[number, number]>([0, 0]);
+  const nodes = useRef<{ [key: string]: MeshNode }>({});
+  const setNode = (key: string) => (node: MeshNode) => {
     nodes.current[key] = node;
   };
 
   const life = useLife({
-    size: 50,
-    fps: 3,
+    size: 80,
+    fps: 12,
     eventHandler: updateCell(nodes),
   });
 
   return (
-    <div>
-      <div>
-        <label>FPS:</label>
-        <input
-          type="range"
-          min="1"
-          max="33"
-          value={life.fps}
-          onChange={(e) => {
-            life.setFps(Number(e.target.value));
-          }}
-        />
-        ({life.fps})
+    <div style={{ height: "100vh" }}>
+      <Canvas
+        camera={{ position: [0, 0, 65] }}
+        onMouseMove={(e) =>
+          (mouse.current = [
+            e.clientX - window.innerWidth / 2,
+            e.clientY - window.innerHeight / 2,
+          ])
+        }
+      >
+        <Rig mouse={mouse} />
+        <ambientLight />
+        <pointLight position={[0, 0, 150]} intensity={0.55} />
+        <group position={[-life.size / 2, -life.size / 2, 0]}>
+          {R.range(0, life.size).map((i) =>
+            R.range(0, life.size).map((j) => (
+              <Cell
+                key={`${i}-${j}`}
+                position={[i, j, 0]}
+                cellRef={setNode(`${i}-${j}`)}
+              />
+            ))
+          )}
+        </group>
+      </Canvas>
+      <div style={{ position: "absolute", top: "1rem" }}>
+        <div>
+          <label>FPS:</label>
+          <input
+            type="range"
+            min="1"
+            max="33"
+            value={life.fps}
+            onChange={(e) => {
+              life.setFps(Number(e.target.value));
+            }}
+          />
+          ({life.fps})
+        </div>
+        <div>
+          <label>Size:</label>
+          <input
+            type="range"
+            min="30"
+            max="200"
+            step="10"
+            value={life.size}
+            onChange={(e) => {
+              life.setSize(Number(e.target.value));
+            }}
+          />
+          ({life.size})
+        </div>
       </div>
-      <div>
-        <label>Size:</label>
-        <input
-          type="range"
-          min="30"
-          max="200"
-          step="10"
-          value={life.size}
-          onChange={(e) => {
-            life.setSize(Number(e.target.value));
-          }}
-        />
-        ({life.size})
-      </div>
-      {
-        <table style={{ borderCollapse: "collapse" }}>
-          <tbody>
-            {R.range(0, life.size).map((i) => (
-              <tr key={i}>
-                {R.range(0, life.size).map((j) => (
-                  <Cell key={`${i}-${j}`} cellRef={setNode(`${i}-${j}`)} />
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      }
     </div>
   );
 };
 
-type CellProps = {
-  cellRef: React.Ref<HTMLElement>;
+const Rig = ({
+  mouse,
+}: {
+  mouse: React.MutableRefObject<[number, number]>;
+}) => {
+  const { camera } = useThree();
+  useFrame(() => {
+    camera.position.x += (mouse.current[0] / 50 - camera.position.x) * 0.05;
+    camera.position.y += (-mouse.current[1] / 50 - camera.position.y) * 0.05;
+    /* camera.lookAt(0, 0, 0); */
+  });
+  return null;
 };
 
-const Cell = ({ cellRef }: CellProps) => {
+type CellProps = {
+  position: [number, number, number];
+  cellRef: React.Ref<MeshNode>;
+};
+
+const Cell = ({ cellRef, position }: CellProps) => {
   return (
-    <td
-      style={{
-        width: "0.5rem",
-        minWidth: "0.5rem",
-        height: "0.5rem",
-        minHeight: "0.5rem",
-      }}
-      ref={cellRef as React.Ref<HTMLTableCellElement>}
-    ></td>
+    <mesh ref={cellRef as React.Ref<Mesh>} position={position} visible={false}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" />
+    </mesh>
   );
 };
 
@@ -160,7 +188,7 @@ type CellData = {
 type Event = { Died: CellData } | { Born: CellData };
 
 const updateCell = (
-  refs: MutableRefObject<{ [key in string]: HTMLElement | undefined }>
+  refs: MutableRefObject<{ [key in string]: JSX.IntrinsicElements["mesh"] }>
 ) => (e: Event) => {
   const data = "Died" in e ? e.Died : e.Born;
   const element = refs.current[`${data.coords[0]}-${data.coords[1]}`];
@@ -168,11 +196,25 @@ const updateCell = (
   if (!element) {
     return;
   }
+
   if ("Died" in e) {
-    element.style.backgroundColor = "transparent";
+    element.visible = false;
   } else {
-    element.style.backgroundColor = `rgb(${data.data.map((c) => c * 255)})`;
+    element.visible = true;
+    setMeshColor(element, new Color(...data.data));
   }
+};
+
+const setMeshColor = (mesh: MeshNode, color: Color) => {
+  if (!mesh.material) {
+    return;
+  }
+
+  const material = Array.isArray(mesh.material)
+    ? mesh.material[0]
+    : mesh.material;
+
+  (material as MeshStandardMaterial).color = color;
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
